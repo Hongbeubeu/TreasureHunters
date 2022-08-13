e52595c43c17f306e84d2d58e7702ecbf348dba9
hongnv <hongnv@iecgames.net>
2022-08-13 15:53:12 +0700

    Update

---
+++ Assets/_TreasureHunters/Scripts/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
